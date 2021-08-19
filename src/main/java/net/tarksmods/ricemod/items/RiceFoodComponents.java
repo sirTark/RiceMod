84a2f45fe6e7db5e87a6d0f1a231a9d2467fbec1
Tark <connorjamessmith@gmail.com>
2021-08-19 04:43:57 +0100

    Renamed from ModFoodComponents.java

--- src/main/java/net/tarksmods/ricemod/items/RiceFoodComponents.java
+++ src/main/java/net/tarksmods/ricemod/items/RiceFoodComponents.java
@@ -2,7 +2,7 @@ package net.tarksmods.ricemod.items;
 
 import net.minecraft.item.FoodComponent;
 
-public class ModFoodComponents {
+public class RiceFoodComponents {
 
     public static final FoodComponent RICE_BALL = (new FoodComponent.Builder().hunger(1).saturationModifier(.6f).build());
     public static final FoodComponent SUSHI = (new FoodComponent.Builder().hunger(2).saturationModifier(2.0f).build());
