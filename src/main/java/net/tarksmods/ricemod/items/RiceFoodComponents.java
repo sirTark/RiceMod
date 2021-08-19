8e308bbfcb223914d13b527069e73cbe7de46f4f
Tark <connorjamessmith@gmail.com>
2021-08-19 23:59:49 +0100

    Raised Food Item stats a little.

--- src/main/java/net/tarksmods/ricemod/items/RiceFoodComponents.java
+++ src/main/java/net/tarksmods/ricemod/items/RiceFoodComponents.java
@@ -4,7 +4,6 @@ import net.minecraft.item.FoodComponent;
 
 public class RiceFoodComponents {
 
-    public static final FoodComponent RICE_BALL = (new FoodComponent.Builder().hunger(1).saturationModifier(.6f).build());
-    //TODO: Fill more hunger bars. 2.5?
-    public static final FoodComponent SUSHI = (new FoodComponent.Builder().hunger(2).saturationModifier(2.0f).build());
+    public static final FoodComponent RICE_BALL = (new FoodComponent.Builder().hunger(3).saturationModifier(.6f).build());
+    public static final FoodComponent SUSHI = (new FoodComponent.Builder().hunger(8).saturationModifier(1.6f).build());
 }
