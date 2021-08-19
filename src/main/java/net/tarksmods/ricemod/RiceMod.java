07a87a7ad32ac506080598e3817252932a1c2005
Tark <connorjamessmith@gmail.com>
2021-08-19 04:46:36 +0100

    Implemented a new class and function to init.

--- src/main/java/net/tarksmods/ricemod/RiceMod.java
+++ src/main/java/net/tarksmods/ricemod/RiceMod.java
@@ -1,19 +1,20 @@
 package net.tarksmods.ricemod;
 
 import net.fabricmc.api.ModInitializer;
-import net.tarksmods.ricemod.blocks.ModBlocks;;
-import net.tarksmods.ricemod.items.ModItems;
-
-
+import net.tarksmods.ricemod.blocks.RiceBlocks;
+import net.tarksmods.ricemod.items.RiceItems;
+import net.tarksmods.ricemod.utils.RiceLootTables;
 public class RiceMod implements ModInitializer {
 
+
+    // Mod ID
     public static final String MOD_ID = "ricemod";
 
     @Override
     public void onInitialize() {
-        ModItems.registerItems();
-        ModBlocks.registerBlocks();
-
-
+        RiceItems.registerItems();
+        RiceBlocks.registerBlocks();
+        RiceLootTables.modifyLootTables();
     }
+
 }
