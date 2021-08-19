38f8ddc2b54cd7c502fcde33e66711350c1cadea
Tark <connorjamessmith@gmail.com>
2021-08-20 00:10:05 +0100

    Optimised Imports

--- src/main/java/net/tarksmods/ricemod/items/RiceItems.java
+++ src/main/java/net/tarksmods/ricemod/items/RiceItems.java
@@ -3,7 +3,6 @@ package net.tarksmods.ricemod.items;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.AliasedBlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.tarksmods.ricemod.RiceMod;
