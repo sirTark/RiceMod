df326fa08d0c641a14169821e49fd8881e236e86
Tark <connorjamessmith@gmail.com>
2021-08-19 04:45:58 +0100

    Adds the rice seeds to the creeper loot tables, there is a one percent change for a seed to drop when killing a creeper, just like zombies with carrots and potatoes.

---
+++ src/main/java/net/tarksmods/ricemod/utils/RiceLootTables.java
@@ -0,0 +1,29 @@
+package net.tarksmods.ricemod.utils;
+
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
+import net.minecraft.util.Identifier;
+import net.tarksmods.ricemod.items.RiceItems;
+
+public class RiceLootTables {
+
+
+    // Create an Identifier for the loot table you want to change.
+    private static final Identifier CREEPER_LOOT_TABLE_ID = new Identifier("minecraft","entities/creeper");
+
+
+    // Creating the method that will modify the existing loot table.
+    public static void modifyLootTables(){
+        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+
+            if(CREEPER_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(BinomialLootNumberProvider.create(1,0.01f))
+                        .withEntry(ItemEntry.builder(RiceItems.RICE_SEED_ITEM).build());
+                supplier.withPool(poolBuilder.build());
+            }
+        }));
+    }
+}
